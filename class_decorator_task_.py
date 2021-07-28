3907725815abd5b6f6c70e32194dac46618a0b37
Anton Navarka <anton_navarka@epam.com>
2021-07-28 20:35:04 +0300

    done

--- class_decorator_task_.py
+++ class_decorator_task_.py
@@ -9,17 +9,30 @@ Else:
 If class already has attribute with such name:
     do not replace original one but print a warning with module warnings
 """
+import builtins
+import warnings
+
+
 def classdecorator(**kwargs):
     def wrapper(cls):
         for key, value in kwargs.items():
-            if getattr(cls,key, None):
-                ...
+            if getattr(cls, key, None):
+                warnings.warn(f"{key} already in {cls.__name__}")
             else:
-                setattr(cls,key,value)
-
-
+                if isinstance(value, str) or callable(value):
+                    setattr(cls, key, value)
+                else:
+                    raise Exception("value is not a function nor string")
+        return cls
     return wrapper
+
+
 if __name__ == "__main__":
-    @classdecorator(a=2, b=lambda x:x+2, c = 3)
+    @classdecorator(a="s", w=lambda: 2, s=lambda x:x+2)
     class A:
-        c=1
+        w = "sss"
+
+
+    print(A.w)
+    print(A.a)
+    print(A.s(2))
